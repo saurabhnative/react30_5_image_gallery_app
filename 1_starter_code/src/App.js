8e76a59a70bd012b367b37bdebe8fc2026752297
codeclassifiers <codeclassifiers@gmail.com>
2020-10-08 07:03:19 +0530

    Refactored code

---
+++ 1_starter_code/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar navbar-dark bg-dark">
+      <div className="w-100 text-light">Image Gallery</div>
+      </nav>
+      <div className="row">
+      </div>
+    </div>
+  );
+}
+
+export default App;
